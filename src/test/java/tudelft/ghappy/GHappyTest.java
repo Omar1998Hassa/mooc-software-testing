288d73c407c4f626c383ebb1a6cafd077dd0908a
OmarHassan <omarz1998hassan@gmail.com>
2021-05-27 02:47:59 +0200

    mirror and ghappy

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,46 @@
 package tudelft.ghappy;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 
 public class GHappyTest {
+    @Test
+    public void oneSingular()
+    {
+        GHappy gHappy = new GHappy();
+        boolean result = gHappy.gHappy("xxgxx");
+        Assertions.assertEquals(false, result);
+    }
+
+    @Test
+    public void oneDoubble()
+    {
+        GHappy gHappy = new GHappy();
+        boolean result = gHappy.gHappy("xxggxx");
+        Assertions.assertEquals(true, result);
+    }
+
+    @Test
+    public void oneDoubleOneSingular()
+    {
+        GHappy gHappy = new GHappy();
+        boolean result = gHappy.gHappy("xxggyygxx");
+        Assertions.assertEquals(false, result);
+    }
+    @Test
+    public void shortdouble()
+    {
+        GHappy gHappy = new GHappy();
+        boolean result = gHappy.gHappy("gg");
+        Assertions.assertEquals(true, result);
+    }
+    @Test
+    public void shortSingle()
+    {
+        GHappy gHappy = new GHappy();
+        boolean result = gHappy.gHappy("g");
+        Assertions.assertEquals(false, result);
+    }
+
+
 }
--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,35 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MirrorTest {
+    @Test
+    public void evenMirror() {
+        Mirror mirror = new Mirror();
+        String result = mirror.mirrorEnds("abcdba");
+        Assertions.assertEquals("ab", result);
+    }
+    @Test
+    public void oddMirror() {
+        Mirror mirror = new Mirror();
+        String result = mirror.mirrorEnds("abcdeba");
+        Assertions.assertEquals("ab", result);
+    }
+    @Test
+    public void oddString() {
+        Mirror mirror = new Mirror();
+        String result = mirror.mirrorEnds("abcba");
+        Assertions.assertEquals("abcba", result);
+    }
+
+    @Test
+    public void evenString() {
+        Mirror mirror = new Mirror();
+        String result = mirror.mirrorEnds("abccba");
+        Assertions.assertEquals("abccba", result);
+    }
+
+
+
 }
